e300279adb0769bbb394fd252963bd7b2c42947f
Paul <jpdcands211@gmail.com>
2022-07-06 08:39:25 -0400

    Added Entity & Dao

---
+++ app/src/main/java/com/ebookfrenzy/roompractice/VerticalTractionDao.kt
@@ -0,0 +1,18 @@
+package com.ebookfrenzy.roompractice
+
+import androidx.room.*
+import kotlinx.coroutines.flow.Flow
+
+@Dao
+interface VerticalTractionDao {
+
+    @Insert
+    suspend fun insert(VerticalTraction: VerticalTraction)
+
+    @Update
+    suspend fun update(VerticalTraction: VerticalTraction)
+
+    @Delete
+    suspend fun delete(VerticalTraction: VerticalTraction)
+    
+}
---
+++ app/src/main/java/com/ebookfrenzy/roompractice/VerticalTractionRoomDatabase.kt
@@ -0,0 +1,4 @@
+package com.ebookfrenzy.roompractice
+
+class VerticalTractionRoomDatabase {
+}
