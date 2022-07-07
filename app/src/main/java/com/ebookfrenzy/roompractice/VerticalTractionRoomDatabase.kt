bc205429ecce4696fc5f5428ca05f762093de5d3
Paul <jpdcands211@gmail.com>
2022-07-07 12:54:05 -0400

    Up to HFAD pg. 591

--- app/src/main/java/com/ebookfrenzy/roompractice/VerticalTractionRoomDatabase.kt
+++ app/src/main/java/com/ebookfrenzy/roompractice/VerticalTractionRoomDatabase.kt
@@ -6,31 +6,31 @@ import androidx.room.Room
 import androidx.room.RoomDatabase
 
 // Annotates class to be a Room Database with a table (entity) of the Word class
-@Database(entities = arrayOf(VerticalTraction::class), version = 1, exportSchema = false)
-public abstact class VerticalTractionRoomDatabase : RoomDatabase() {
 
-    abstract fun wordDao(): VerticalTractionDao
+@Database(entities = [VerticalTraction::class], version = 1, exportSchema = false)
+abstact class VerticalTractionRoomDatabase : RoomDatabase() {
+    abstract val verticalTractionDao: VerticalTractionDao
 
     companion object {
-        // Singleton prevents multiple instances of database opening at the
-        // same time.
+        // Singleton prevents multiple instances of database opening at the same time.
         @Volatile
         private var INSTANCE: VerticalTractionRoomDatabase? = null
 
-        fun getDatabase(context: Context): VerticalTractionRoomDatabase {
-            // if the INSTANCE is not null, then return it,
-            // if it is, then create the database
-            return INSTANCE ?: synchronized(this) {
-                val instance = Room.databaseBuilder(
-                    context.applicationContext,
-                    VerticalTractionRoomDatabase::class.java,
-                    "word_database"
-                ).build()
-                INSTANCE = instance
+        fun getInstance(context: Context): VerticalTractionRoomDatabase {
+        // if the INSTANCE is not null, then return it, if it is, then create the database
+            synchronized(this) {
+                var instance = INSTANCE
+                if (instance == null) {
+                    instance = Room.databaseBuilder(
+                        context.applicationContext,
+                        VerticalTractionRoomDatabase::class.java,
+                        "verticalTraction_database"
+                    ).build()
+                    INSTANCE = instance
+                }
                 // return instance
                 instance
             }
         }
     }
-}
 }
