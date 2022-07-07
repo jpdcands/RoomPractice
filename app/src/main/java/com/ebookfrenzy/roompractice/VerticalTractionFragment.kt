50e598940f1ebcd7d0acead1fba5066fc31fa85d
Paul <jpdcands211@gmail.com>
2022-07-07 12:56:31 -0400

    Up to HFAD pg. 591

---
+++ app/src/main/java/com/ebookfrenzy/roompractice/VerticalTractionFragment.kt
@@ -0,0 +1,28 @@
+package com.ebookfrenzy.roompractice
+
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.lifecycle.ViewModelProvider
+import com.ebookfrenzy.roompractice.databinding.FragmentVerticalTractionBinding
+
+class TasksFragment : Fragment() {
+    private var _binding: FragmentVerticalTractionBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentVerticalTractionBinding.inflate(inflater, container, false)
+        val view = binding.root
+        return view
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
